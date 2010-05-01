790cbafa799104a29f0063c6c749e85e496e4b29
leo.christ <leo.christ@hotmail.com>
2010-05-01 06:36:09 +0000

    修改jpa 已经可以正常运行 但是 对于 onetomany 和 manytoone 的级联关系 还没有掌握

--- lims/src/cn/edu/buaa/leochrist/model/Team.java
+++ lims/src/cn/edu/buaa/leochrist/model/Team.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -12,8 +13,11 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
 @SuppressWarnings("serial")
+@Entity
+@Table(name = "team")
 public class Team implements Serializable {
 
 	private Integer id;
--- lims/src/cn/edu/buaa/leochrist/model/WorkStatus.java
+++ lims/src/cn/edu/buaa/leochrist/model/WorkStatus.java
@@ -4,14 +4,18 @@ import java.io.Serializable;
 import java.util.Date;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 @SuppressWarnings("serial")
+@Entity
+@Table(name = "work_status")
 public class WorkStatus implements Serializable {
 
 	private Integer id;
